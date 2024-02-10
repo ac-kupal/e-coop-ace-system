952824bb2f20d5dfa3bad8f97495bab0d98e10f8
Jervx <louellagracechua@gmail.com>
2024-02-10 13:26:00 +0800

    chore: Updated middlware & schema

--- middleware.ts
+++ middleware.ts
@@ -2,9 +2,8 @@ import { withAuth } from "next-auth/middleware";
 
 export default withAuth((req)=>{
     const { nextUrl } = req;
-    console.log("auth", req.nextauth.token)
 })
 
 export const config = {
-    matcher: ["/admin"],
+    matcher: ["/admin/:path*"],
 }
--- services/initialize-default.ts
+++ services/initialize-default.ts
@@ -3,16 +3,16 @@ import db from "@/lib/database"
 export const INIT_ROOT_ACCOUNT = async () => {
     return await db.user.create({
         data : {
-            email : "initialacc@gmail.com",
-            name : "initial root",
+            email : "cooperatives.cloud@gmail.com",
+            name : "Coop Cloud",
             password : "$2a$10$iJFu3JszgpMfmDuqrSoD/e7XzVJdpEJRMQaPJFnAvAYo6xBLTiOeC",
             verified : true,
             branch : {
                 create : {
-                    branchName : "initial branch",
-                    branchAddress : "initial address",
+                    branchName : "main branch",
+                    branchAddress : "main address",
                     branchPicture : "/images/default.png",
-                    branchDescription : "this is just an initial branch"
+                    branchDescription : "this is just an dummy branch"
                 }
             },
             roles : {
