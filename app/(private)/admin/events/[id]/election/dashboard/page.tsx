ee0e61ff521f55604a6a2b3839c255150c1fc344
dctrrohan@gmail.com <dctrrohan@gmail.com>
2024-02-20 22:09:38 +0800

    fix: router path in election dashboard
    removed settings.json

--- app/(private)/admin/events/[id]/election/dashboard/page.tsx
+++ app/(private)/admin/events/[id]/election/dashboard/page.tsx
@@ -1,8 +1,40 @@
+"use client"
+import { mutationErrorHandler } from '@/errors/mutation-error-handler';
+import { TEventWithElection } from '@/types/event/TCreateEvent';
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
 import React from 'react'
+import Election from '../_components/election';
+import NotFound from '../_components/not-found';
 
-const page = () => {
+const page = ({ params }: { params: { id: number } }) => {
+
+  const { data, isLoading } = useQuery<TEventWithElection>({
+    queryKey: ["get-event-key"],
+    queryFn: async () => {
+       try {
+          const response = await axios.get(`/api/v1/event/${params.id}`);
+          return response.data;
+       } catch (error) {
+          mutationErrorHandler(error);
+       }
+    },
+ });
+
   return (
-    <div>Dashboard</div>
+    <div>
+        <h1 className="text-2xl font-semibold">Manage Election</h1>
+         {data?.election ? (
+            <>
+               <Election
+                  isLoading={isLoading}
+                  election={data.election}
+               ></Election>
+            </>
+         ) : (
+            <NotFound></NotFound>
+         )}
+    </div>
   )
 }
 
--- app/(private)/admin/events/[id]/election/layout.tsx
+++ app/(private)/admin/events/[id]/election/layout.tsx
@@ -1,18 +1,15 @@
 "use client";
 import React, { ReactNode } from "react";
-import { TElectionRoute } from "@/types";
-import { LayoutDashboard, Users, Medal, Settings2 } from "lucide-react";
 import ElectionSideBar from "./_components/election-sidebar";
 
 interface Props {
    children?: ReactNode;
 }
 
-
 const layout = ({ children }: Props) => {
    return (
       <div className="flex p-5 shadow-md rounded-xl">
-         <ElectionSideBar/>
+         <ElectionSideBar />
          <div className="border w-full bg-background ">{children}</div>
       </div>
    );
