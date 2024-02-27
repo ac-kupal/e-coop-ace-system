ace1f15a2f29d686a07ad66b21da84d42dabcc85
Jervx <louellagracechua@gmail.com>
2024-02-27 09:40:28 +0800

    feat: Applied birthday verification method

--- app/(public)/events/[id]/election/_components/invalid-election.tsx
+++ app/(public)/events/[id]/election/_components/invalid-election.tsx
@@ -1,6 +1,6 @@
 const InvalidElection = ({message = "invalid election"} : { message? : string }) => {
     return (
-        <div className="h-dvh w-dvw flex flex-col items-center justify-center">
+        <div className="flex-1 w-dvw flex flex-col items-center justify-center">
             <p className="text-sm text-center text-foreground/70">{message}</p>
         </div>
     );
