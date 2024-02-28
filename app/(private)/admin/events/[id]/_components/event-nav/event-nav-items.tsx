d5e373ed70758009fcfe70dc6ce68855dae8bb6c
dctrrohan@gmail.com <dctrrohan@gmail.com>
2024-02-28 12:26:39 +0800

    chore: update event nav

--- app/(private)/admin/events/[id]/_components/event-nav/event-nav-items.tsx
+++ app/(private)/admin/events/[id]/_components/event-nav/event-nav-items.tsx
@@ -15,8 +15,8 @@ const EventNavItems = ({ route}: Props) => {
 
    return (
       <Link href={`${path}`} className="flex cursor-pointer justify-start space-x-2">
-         <div className={`flex space-x-2 duration-300  ease-in-out ${isCurrentPath ? "  text-[#099065] font-bold":"text-foreground/90 "} px-5  hover:bg-white dark:hover:bg-primary/30 shadow-sm p-2 rounded-xl`}>
-            <div className="">
+         <div className={`flex space-x-2 duration-300  ease-in-out ${isCurrentPath ? "  text-[#099065] font-bold":"text-foreground/90 "} px-5  hover:bg-[#17f5af]/30 dark:hover:bg-primary/30 shadow-sm p-2 rounded-xl`}>
+            <div className={`${isCurrentPath ? "  dark:text-[#17f5af] font-bold":"text-foreground/90 "}`}>
                {icon}
             </div>
             <h1 className="">{name}</h1>
