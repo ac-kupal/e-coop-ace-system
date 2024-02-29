8a537a26ef88412e6dae996b95e0a6cd16081534
Jervx <louellagracechua@gmail.com>
2024-02-29 21:27:45 +0800

    fix: Centered message when election error

--- app/(public)/events/[id]/election/page.tsx
+++ app/(public)/events/[id]/election/page.tsx
@@ -5,30 +5,41 @@ import InvalidElection from "./_components/invalid-election";
 import ValidateVoter from "./_components/validate-voter";
 
 type Props = {
-    params : { id : number }
+    params: { id: number };
 };
 
 const ElectionVerifyPage = async ({ params }: Props) => {
-   
-    const validatedEventId = eventIdParamSchema.safeParse(params.id)    
+    const validatedEventId = eventIdParamSchema.safeParse(params.id);
 
-    if(!validatedEventId.success) return <InvalidElection />
+    if (!validatedEventId.success) return <InvalidElection />;
 
-    const election = await db.election.findUnique({ 
-        where : { eventId : validatedEventId.data },
-        include : { event : true }
-    }) 
-    
-    if(!election) return <InvalidElection />
+    const election = await db.election.findUnique({
+        where: { eventId: validatedEventId.data },
+        include: { event: true },
+    });
 
-    if(election.status === "done") return <InvalidElection message="This election has ended" />
-    
-    if (election.status != "live") return <InvalidElection message="Election is not yet open" />
+    if (!election) return <InvalidElection />;
+
+    if (election.status === "done")
+        return (
+            <div className="flex flex-col py-20 px-5 gap-y-6 min-h-screen w-full items-center">
+                <InvalidElection message="This election has ended" />
+            </div>
+        );
+
+    if (election.status != "live")
+        return (
+            <div className="flex flex-col py-20 px-5 gap-y-6 min-h-screen w-full items-center">
+                <InvalidElection message="Election is not yet open" />
+            </div>
+        );
 
     return (
         <div className="flex flex-col py-20 px-5 gap-y-6 min-h-screen w-full items-center">
-            <p className="text-2xl lg:text-4xl uppercase text-center">{election.electionName}</p>
-            <div className="w-5 h-2 bg-orange-400 rounded-full"/>
+            <p className="text-2xl lg:text-4xl uppercase text-center">
+                {election.electionName}
+            </p>
+            <div className="w-5 h-2 bg-orange-400 rounded-full" />
             <div className="py-16">
                 <ValidateVoter electionWithEvent={election} />
             </div>
