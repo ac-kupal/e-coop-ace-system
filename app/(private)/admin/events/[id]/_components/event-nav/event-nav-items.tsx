14307ccc8252181100c6ca72e7353a125d733705
dctrrohan@gmail.com <dctrrohan@gmail.com>
2024-03-11 21:03:21 +0800

    feat: event header and some details

--- app/(private)/admin/events/[id]/_components/event-nav/event-nav-items.tsx
+++ app/(private)/admin/events/[id]/_components/event-nav/event-nav-items.tsx
@@ -17,9 +17,9 @@ const EventNavItems = ({ route, eventId }: Props) => {
    const finalPath = `/admin/events/${eventId}/${path}`
 
    return (
-      <Link href={finalPath} className="flex cursor-pointer justify-start space-x-2  hover:scale-105">
-         <div className={`flex lg:space-x-2 duration-300  items-center px-2 lg:px-5  ease-in-out ${isCurrentPath ? "text-[#099065] font-bold":"text-foreground/90 "}  hover:bg-non dark:bg-gray-600/20 border-b p-2 rounded-xl`}>
-            <div className={`${isCurrentPath ? "  dark:text-[#17f5af] font-bold":"text-foreground/90 "} hidden lg:block `}>
+      <Link href={finalPath} className={` ${isCurrentPath ? "text-primary font-bold ":" text-muted-foreground/70 hover:text-[#167965] hover:font-bold "}   border-primary flex cursor-pointer justify-start space-x-2  hover:scale-105`}>
+         <div className={`flex lg:space-x-2 duration-300  items-center px-2 lg:px-5  ease-in-out hover:bg-none dark:bg-gray-600/20  p-2 rounded-xl`}>
+            <div className={` hidden lg:block `}>
                {icon}
             </div>
             <h1 className={`text-[min(16px,3.2vw)] ${isCurrentPath ? "font-bold":"font-normal"}`}>{name}</h1>
