0d6b91ca7d9d860fc02d9959fc76b6d19f9fc535
dctrrohan@gmail.com <dctrrohan@gmail.com>
2024-03-11 21:03:21 +0800

    feat: event header and some details

--- app/(private)/admin/events/[id]/_components/event-nav/index.tsx
+++ app/(private)/admin/events/[id]/_components/event-nav/index.tsx
@@ -1,63 +1,68 @@
-import {  TEventWithElection, TNavListRoute } from "@/types";
+import { TEventWithElection, TNavListRoute } from "@/types";
 import {
-    BaggageClaim,
-    Gift,
-    ListChecks,
-    Users,
-    Vote,
-    Wrench,
+   BaggageClaim,
+   Gift,
+   ListChecks,
+   Users,
+   Vote,
+   Wrench,
 } from "lucide-react";
 import React from "react";
 import EventNavItems from "./event-nav-items";
+import Header from "../../election/[electionId]/_components/header";
 
 export const EventRoutes: TNavListRoute[] = [
-    {
-        icon: <Users className="size-5" />,
-        name: "Members",
-        path: "manage-member",
-    },
-    {
-        icon: <ListChecks className=" size-5" />,
-        name: "Attendance",
-        path: "attendance",
-    },
-    {
-        icon: <Gift className="size-5" />,
-        name: "Incentive",
-        path: "incentive",
-    },
-    {
-        icon: <Wrench className="size-5" />,
-        name: "Event Settings",
-        path: "event-settings",
-    }
+   {
+      icon: <Users className="size-5" />,
+      name: "Members",
+      path: "manage-member",
+   },
+   {
+      icon: <ListChecks className=" size-5" />,
+      name: "Attendance",
+      path: "attendance",
+   },
+   {
+      icon: <Gift className="size-5" />,
+      name: "Incentive",
+      path: "incentive",
+   },
+   {
+      icon: <Wrench className="size-5" />,
+      name: "Event Settings",
+      path: "event-settings",
+   },
 ];
 
 type Props = {
-    hasElection: boolean;
+   hasElection: boolean;
 };
 
 const EventNav = ({ event }: { event: TEventWithElection }) => {
-    const finalRoutes: TNavListRoute[] = [
-        ...EventRoutes,
-        ...(event.election
-            ? [
-                  {
-                      icon: <Vote className="size-5" />,
-                      name: "Election",
-                      path: `election/${event.election.id}`,
-                  },
-              ]
-            : []),
-    ];
+   const EventNavRoutes: TNavListRoute[] = [
+      ...EventRoutes,
+      ...(event.election
+         ? [
+              {
+                 icon: <Vote className="size-5" />,
+                 name: "Election",
+                 path: `election/${event.election.id}`,
+              },
+           ]
+         : []),
+   ];
 
-    return (
-        <div className="flex space-x-1 w-full justify-evenly lg:justify-start ">
-            {finalRoutes.map((route: TNavListRoute, i) => {
-                return <EventNavItems eventId={event.id} route={route} key={i} />;
+   return (
+      <div>
+         <div className="flex space-x-1 w-full justify-evenly lg:justify-start ">
+            {EventNavRoutes.map((route: TNavListRoute, i) => {
+               return (
+                  <EventNavItems eventId={event.id} route={route} key={i} />
+               );
             })}
-        </div>
-    );
+         </div>
+      </div>
+   );
 };
 
 export default EventNav;
--- app/(private)/admin/events/[id]/layout.tsx
+++ app/(private)/admin/events/[id]/layout.tsx
@@ -4,6 +4,9 @@ import React, { ReactNode } from "react";
 import NotFound from "./_components/not-found";
 import EventNav from "./_components/event-nav";
 import { eventIdSchema } from "@/validation-schema/commons";
+import { Separator } from "@/components/ui/separator";
+import { CalendarCheck, CalendarDays, MapPin, TrainFrontTunnel } from "lucide-react";
+import moment from "moment";
 
 type Props = {
    children: ReactNode;
@@ -22,12 +25,38 @@ const EventLayout = async ({ children, params }: Props) => {
 
    
    return (
-      <div className="font-poppins pt-5 lg:p-7 h-fit overflow-hidden">
-         <div className="p-5 w-full">
-           <EventNav event={event} />
+      <div className="font-poppins pt-5 lg:p-7 space-y-4 h-fit overflow-hidden">
+         <div className="p-5 w-full rounded-2xl flex flex-col lg:flex-row space-x-0 space-y-3 lg:space-x-16 lg:space-y-0  justify-start shadow-md bg-background dark:bg-secondary/30">
+            <div className="flex space-x-2 items-center ">
+               <div className="bg-[#22C55E] rounded-lg p-1">
+                  <CalendarCheck className="text-primary" />
+               </div>
+               <h1 className="text-lg font-bold text-black/80 dark:text-white/80">{event.title} New Boarders Lorem 2024</h1>
+            </div>
+            <div className="flex space-x-2 items-center ">
+               <div className="dark:bg-yellow-500 bg-yellow-400   rounded-lg p-1">
+                  <MapPin className="text-yellow-700 dark:text-white " />
+               </div>
+               <h1 className="text-lg font-normal text-black/80 dark:text-white/80">
+                  {event.location}
+               </h1>
+            </div>
+            <div className="flex space-x-2 items-center">
+               <div className="bg-blue-500 dark:bg-blue-800 rounded-lg p-1">
+               <CalendarDays className="text-blue-800 dark:text-white" />
+               </div>
+               <h1 className="text-lg font-normal text-black/80 dark:text-white/80">
+                  {moment(event.date).format("LL")}
+               </h1>
+            </div>
+
          </div>
-         <div className="flex bg-background border border-[#00000012] min-h-screen shadow-xl dark:bg-secondary/30 py-4 rounded-3xl overflow-x-hidden lg:p-2  w-full ">
-               {children}
+         <div className="flex flex-col bg-background  border-[#00000012] min-h-screen shadow-xl dark:bg-secondary/30 py-4 rounded-3xl space-y-2 overflow-x-hidden lg:p-5  w-full ">
+            <div className="p-5 w-full">
+               <EventNav event={event} />
+               <Separator className="dark:hidden"></Separator>
+            </div>
+            <div>{children}</div>
          </div>
       </div>
    );
