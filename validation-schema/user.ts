25ac87be37795a7b858052264aa6decae4af43e9
Jervx <louellagracechua@gmail.com>
2024-03-18 12:24:29 +0800

    feat: Add claim report api endpoint

--- validation-schema/user.ts
+++ validation-schema/user.ts
@@ -1,7 +1,16 @@
 import { Role } from "@prisma/client";
 import z from "zod";
 
-export const roleEnum = z.nativeEnum(Role, { invalid_type_error : "invalid role" })
+export const roleEnum = z.nativeEnum(Role, {
+    invalid_type_error: "invalid role",
+});
+
+export const userIdSchema = z.coerce.number({
+    invalid_type_error: "invalid id",
+    required_error: "ids is required",
+});
+
+export const userIdsSchema = z.array(userIdSchema);
 
 export const createUserSchema = z.object({
     // picture : z.string().min(5, "invalid image URL").optional(),
@@ -25,12 +34,17 @@ export const createUserSchema = z.object({
         })
         .min(8, "minimum password is 8 character")
         .max(15, "maximum password size is 15 character"),
-    role : roleEnum,
-    branchId : z.coerce.number({ required_error : "branch is required", invalid_type_error : "invalid branch"}).min(1, "Please select a branch")
+    role: roleEnum,
+    branchId: z.coerce
+        .number({
+            required_error: "branch is required",
+            invalid_type_error: "invalid branch",
+        })
+        .min(1, "Please select a branch"),
 });
 
 export const updateUserSchema = z.object({
-    picture : z.string().min(5, "invalid image URL").optional(),
+    picture: z.string().min(5, "invalid image URL").optional(),
     name: z
         .string({
             required_error: "name is required",
@@ -50,7 +64,13 @@ export const updateUserSchema = z.object({
             invalid_type_error: "invalid password",
         })
         .min(8, "minimum password is 8 character")
-        .max(15, "maximum password size is 15 character").optional(),
-    role : roleEnum,
-    branchId : z.coerce.number({ required_error : "branch is required", invalid_type_error : "invalid branch"}).min(1, "Please select a branch")
+        .max(15, "maximum password size is 15 character")
+        .optional(),
+    role: roleEnum,
+    branchId: z.coerce
+        .number({
+            required_error: "branch is required",
+            invalid_type_error: "invalid branch",
+        })
+        .min(1, "Please select a branch"),
 });
