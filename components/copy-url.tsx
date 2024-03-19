7ca3dad9f9156efb2f8dbe065a8f1364909e7eef
Jervx <louellagracechua@gmail.com>
2024-03-19 12:41:06 +0800

    chore: Added vote QR

--- components/copy-url.tsx
+++ components/copy-url.tsx
@@ -3,16 +3,16 @@ import { Check, Copy, Send } from "lucide-react";
 import React, { useState } from "react";
 import { toast } from "sonner";
 
-type Props = { url : string, className? : string, duration? : number };
+type Props = { url : string, displayText? : string, className? : string, duration? : number };
 
-const CopyURL = ( { url, className, duration = 3}: Props) => {
+const CopyURL = ( { url, displayText="Copy URL", className, duration = 3}: Props) => {
     const [coppied, setCoppied] = useState(false);
 
     const handleCopy = () => {
         if(coppied) return;
         try{
             navigator.clipboard.writeText(url)
-            toast.success("Event link coppied")
+            toast.success("Coppied")
             setCoppied(true)
             setTimeout(()=> setCoppied(false), duration * 1000 )
         }catch(e){
@@ -21,8 +21,8 @@ const CopyURL = ( { url, className, duration = 3}: Props) => {
     }
 
     return (
-        <div onClick={handleCopy} className={cn("flex gap-x-4 px-3 group duration-200 cursor-pointer text-foreground/70 hover:text-foreground py-2 bg-secondary rounded-xl text-sm", className, coppied && "bg-green-300/20")}>
-            <p>{url}</p>
+        <div onClick={handleCopy} className={cn("flex gap-x-4 px-3 text-xs group duration-200 cursor-pointer text-foreground/70 hover:text-foreground py-2 bg-secondary rounded-xl ", className, coppied && "bg-green-300/20")}>
+            <p>{displayText}</p>
             {
                 coppied ? <Check className="size-4 text-green-700" strokeWidth={1} /> : <Copy className="h-4 w-4" strokeWidth={1} />
             }
