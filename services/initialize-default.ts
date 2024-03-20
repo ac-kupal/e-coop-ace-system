6cbaa6fe14513d8f2439da12c4d799866cb8e3ea
Jervx <louellagracechua@gmail.com>
2024-03-20 22:58:25 +0800

    chore: Added coop connection and initial setup coop creation

--- services/initialize-default.ts
+++ services/initialize-default.ts
@@ -1,21 +1,36 @@
-import db from "@/lib/database"
+import db from "@/lib/database";
 
 export const INIT_ROOT_ACCOUNT = async () => {
-    return await db.user.create({
-        data : {
-            email : "cooperatives.cloud@gmail.com",
-            name : "Coop Cloud",
-            password : "$2a$10$iJFu3JszgpMfmDuqrSoD/e7XzVJdpEJRMQaPJFnAvAYo6xBLTiOeC",
-            verified : true,
-            role : "root",
-            branch : {
-                create : {
-                    branchName : "main branch",
-                    branchAddress : "main address",
-                    branchPicture : "/images/default.png",
-                    branchDescription : "this is just an dummy branch"
-                }
+    const newCoop = await db.coop.create({
+        data: {
+            coopName: "Main Cooperatives",
+            coopDescription: "Don't delete this coop as this is used by the root user.",
+        },
+    });
+
+    if (!newCoop) throw new Error("Couldn't initialize root account, try again");
+
+    const newBranch = await db.branch.create({
+        data: {
+            branchName: "main branch",
+            branchAddress: "main address",
+            branchPicture: "/images/default.png",
+            branchDescription: "this is just an dummy branch",
+            coopId: newCoop.id,
+            users: {
+                create: {
+                    email: "cooperatives.cloud@gmail.com",
+                    name: "Coop Cloud",
+                    password: "$2a$10$iJFu3JszgpMfmDuqrSoD/e7XzVJdpEJRMQaPJFnAvAYo6xBLTiOeC",
+                    verified: true,
+                    role: "root",
+                    coopId: newCoop.id
+                },
             },
-        }
-    })
-}
+        },
+    });
+
+    if(!newBranch) throw new Error("Couldn't initialize root account, try again")
+
+    return true;
+};
