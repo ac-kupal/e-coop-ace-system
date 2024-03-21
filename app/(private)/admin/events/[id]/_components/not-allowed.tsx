6558be6d449d3ce9e34782b9f912e285d58cfb31
dctrrohan@gmail.com <dctrrohan@gmail.com>
2024-03-22 00:57:35 +0800

    chore: classname of not allowed components

--- app/(private)/admin/events/[id]/_components/not-allowed.tsx
+++ app/(private)/admin/events/[id]/_components/not-allowed.tsx
@@ -3,12 +3,12 @@ import { ShieldAlert } from "lucide-react";
 import React from "react";
 
 type Props ={
-  clasName?:string,
+  className?:string,
 }
 
-const NotAllowed = ({clasName}:Props) => {
+const NotAllowed = ({className}:Props) => {
    return (
-      <div className={cn("w-full h-screen flex justify-center items-center" + clasName)}>
+      <div className={cn("w-full h-screen flex justify-center items-center" + className)}>
             <div className="flex flex-col items-center gap-2">
                <ShieldAlert className=" text-muted-foreground w-12 h-auto" />
                You are not allowed to visit this page.
