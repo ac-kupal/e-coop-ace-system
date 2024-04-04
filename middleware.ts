48bfcea5529018dd55a99b3683a113715715d43c
Jervx <louellagracechua@gmail.com>
2024-04-04 22:54:08 +0800

    chore: Add admin api paths as protected

--- middleware.ts
+++ middleware.ts
@@ -1,13 +1,12 @@
 import { withAuth } from "next-auth/middleware";
-import { currentUserOrFalse, getServerAuthSession } from "./lib/auth";
-import { getSession } from "next-auth/react";
 import { NextRequest } from "next/server";
 
-export default withAuth((req : NextRequest)=>{
-    const { nextUrl } = req;
-
-})
+export default withAuth((req: NextRequest) => {});
 
 export const config = {
-    matcher: ["/admin/:path*"],
-}
+  matcher: [
+        "/admin/:path*", 
+        "/api/v1/admin/:path*", 
+        "/app/v1/uploade/:path*"
+    ],
+};
