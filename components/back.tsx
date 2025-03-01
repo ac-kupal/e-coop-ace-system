adb2d3652b92a822705184cf0378fba57ff5fdc2
Jervx <louellagracechua@gmail.com>
2025-03-01 08:54:59 +0800

    chore: Trigger build

--- components/back.tsx
+++ components/back.tsx
@@ -1,8 +1,9 @@
 "use client"
-import { Button } from "@/components/ui/button";
+import React from "react";
 import { ArrowLeft } from "lucide-react";
 import { useRouter } from "next/navigation";
-import React from "react";
+
+import { Button } from "@/components/ui/button";
 
 const BackButton = () => {
    const router = useRouter();
