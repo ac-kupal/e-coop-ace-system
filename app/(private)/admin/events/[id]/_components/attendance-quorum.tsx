894da4e489c4ac9d584bbec074ae6628d3035db4
Jervx <louellagracechua@gmail.com>
2025-03-03 15:26:40 +0800

    style: Move quorum in event nav layout

---
+++ app/(private)/admin/events/[id]/_components/attendance-quorum.tsx
@@ -0,0 +1,74 @@
+"use client";
+import React from "react";
+import { toast } from "sonner";
+import { Sigma } from "lucide-react";
+
+import {
+    Card,
+    CardTitle,
+    CardHeader,
+    CardContent,
+    CardDescription,
+} from "@/components/ui/card";
+
+import { useAttendanceStats } from "@/hooks/api-hooks/attendance-api-hooks";
+import LoadingSpinner from "@/components/loading-spinner";
+
+interface Props {
+    eventId: number;
+}
+
+const AttendanceQuorum = ({ eventId }: Props) => {
+    const { data: attendanceStats, isRefetching: isLoadingAttendanceStats } =
+        useAttendanceStats({
+            eventId,
+            onError: () => toast.error("Failed to load event stats"),
+        });
+
+    return (
+        <Card className="w-fit flex-auto relative">
+            <div className="flex p-4 space-x-2  items-center py-2">
+                <div className="p-1 dark:bg-[#c5a522] bg-[#e7c127] rounded-sm">
+                    <Sigma className="size-5 text-slate-200" />
+                </div>
+                <h1 className="font-medium">Quorum</h1>
+            </div>
+            <CardContent className="space-y-2">
+                <div className="flex items-center gap-x-4">
+                    <h1>Total Attendance</h1>
+                    {!attendanceStats && isLoadingAttendanceStats ? (
+                        <>
+                            <p className="text-muted-foreground">...</p>
+                            <LoadingSpinner className="size-6 text-muted-foreground absolute top-4 right-4" />
+                        </>
+                    ) : attendanceStats ? (
+                        <h1 className="font-bold text-3xl text-primary">
+                            {attendanceStats.totalAttendees > 0
+                                ? (
+                                      (attendanceStats.totalIsRegistered /
+                                          attendanceStats.totalAttendees) *
+                                      100
+                                  ).toLocaleString("en-US", {
+                                      maximumFractionDigits: 1,
+                                  })
+                                : "0"}{" "}
+                            %
+                        </h1>
+                    ) : (
+                        <span className="text-3xl text-muted-foreground">
+                            ...
+                        </span>
+                    )}
+                </div>
+
+                <CardDescription className="text-md">
+                    {attendanceStats.totalIsRegistered + " (Total registered)"}
+                    {" / "}
+                    {attendanceStats.totalAttendees + " members"}
+                </CardDescription>
+            </CardContent>
+        </Card>
+    );
+};
+
+export default AttendanceQuorum;
--- app/(private)/admin/events/_components/event-table/index.tsx
+++ app/(private)/admin/events/_components/event-table/index.tsx
@@ -7,11 +7,11 @@ import DataTablePagination from "@/components/data-table/data-table-pagination";
 import DataTableViewOptions from "@/components/data-table/data-table-view-options";
 import columns from "./column";
 import {
-   getCoreRowModel,
-   getFilteredRowModel,
-   getPaginationRowModel,
-   getSortedRowModel,
-   useReactTable,
+    getCoreRowModel,
+    getFilteredRowModel,
+    getPaginationRowModel,
+    getSortedRowModel,
+    useReactTable,
 } from "@tanstack/react-table";
 
 import { cn } from "@/lib/utils";
@@ -24,92 +24,97 @@ import { user } from "next-auth";
 import { Role } from "@prisma/client";
 
 type TParams = {
-   params:{id:number},
-   user:user,
-}
+    params: { id: number };
+    user: user;
+};
 
-const EventTable = ({params,user}:TParams) => {
-   const [globalFilter, setGlobalFilter] = useState<string>("");
-   const [createEvent, setCreateEvent] = useState(false);
-   const onFocusSearch = useRef<HTMLInputElement | null>(null);
+const EventTable = ({ user }: TParams) => {
+    const [globalFilter, setGlobalFilter] = useState<string>("");
+    const [createEvent, setCreateEvent] = useState(false);
+    const onFocusSearch = useRef<HTMLInputElement | null>(null);
 
-   const { data, isFetching, isLoading, isError, refetch } = useGetAllEvent();
-   const table = useReactTable({
-      data,
-      columns,
-      getCoreRowModel: getCoreRowModel(),
-      getPaginationRowModel: getPaginationRowModel(),
-      getSortedRowModel: getSortedRowModel(),
-      getFilteredRowModel: getFilteredRowModel(),
-      state: {
-         globalFilter,
-      },
-      initialState: {
-         pagination: { pageIndex: 0, pageSize: 20 },
-      },
-      onGlobalFilterChange: setGlobalFilter,
-   });
+    const { data, isFetching, isLoading, isError } = useGetAllEvent();
+    const table = useReactTable({
+        data,
+        columns,
+        getCoreRowModel: getCoreRowModel(),
+        getPaginationRowModel: getPaginationRowModel(),
+        getSortedRowModel: getSortedRowModel(),
+        getFilteredRowModel: getFilteredRowModel(),
+        state: {
+            globalFilter,
+        },
+        initialState: {
+            pagination: { pageIndex: 0, pageSize: 20 },
+        },
+        onGlobalFilterChange: setGlobalFilter,
+    });
 
-   return (
-      <div className="flex flex-1 flex-col gap-y-5">
-         <div className="flex justify-between items-center px-5 lg:px-1">
-            <CreateEventModal
-               state={createEvent}
-               user={user}
-               onClose={(state) => setCreateEvent(state)}
-            />
-            <div className="">
-               <h1 className="font-bold text-[min(22px,3.7vw)]">Events</h1>
-               <p className="text-[min(14px,3.4vw)]">manage events and elections</p>
+    return (
+        <div className="flex flex-1 flex-col gap-y-5">
+            <div className="flex justify-between items-center px-5 lg:px-1">
+                <CreateEventModal
+                    state={createEvent}
+                    user={user}
+                    onClose={(state) => setCreateEvent(state)}
+                />
+                <div className="">
+                    <h1 className="font-bold text-[min(22px,3.7vw)]">Events</h1>
+                    <p className="text-[min(14px,3.4vw)]">
+                        manage events and elections
+                    </p>
+                </div>
+                <Button
+                    disabled={user.role === Role.staff}
+                    size="sm"
+                    className={cn(
+                        ` ${user.role === Role.staff ? "invisible" : "visible"} flex rounded-md justify-center items-center md:space-x-2 md:min-w-[7rem]`
+                    )}
+                    onClick={() => setCreateEvent(true)}
+                >
+                    Add Event
+                    <Plus className="w-4 h-4" />
+                </Button>
             </div>
-            <Button
-               disabled={user.role === Role.staff}
-               size="sm"
-               className={cn(
-                  ` ${user.role === Role.staff ? "invisible":"visible"} flex rounded-md justify-center items-center md:space-x-2 md:min-w-[7rem]`
-               )}
-               onClick={() => setCreateEvent(true)}
-            >
-               Add Event
-               <Plus className="w-4 h-4" />
-            </Button>
-         </div>
-         <Card className="h-full border-0 min-h-[70vh] bg-background dark:bg-secondary/30 ">
-            <div className="flex w-full px-5  justify-between pt-5 items-center lg:justify-start space-x-3">
-               <div className="items-center w-full gap-x-4 text-muted-foreground">
-                  <div className="relative">
-                     <SearchIcon className="absolute w-4 h-auto top-3 left-2" />
-                     <Input
-                        ref={onFocusSearch}
-                        placeholder="Search..."
-                        value={globalFilter}
-                        onChange={(event) =>
-                           setGlobalFilter(event.target.value)
-                        }
-                        className=" pl-8 bg-secondary border-0 border-b text-sm md:text-base ring-offset-0 focus-visible:ring-0 focus-visible:ring-offset-0"
-                     />
-                  </div>
-               </div>
-               <div className="">
-                  <DataTableViewOptions className="h-10 " table={table} />
-               </div>
-            </div>
-            <DataTable
-               headerLayout="bg-secondary"
-               className="flex-1 min-h-[70vh] dark:bg-transparent rounded-2xl"
-               isError={isError}
-               isLoading={isLoading || isFetching}
-               table={table}
-            />
-            <div className="lg:hidden p-5">
-               <DataTableBasicPagination2 table={table} />
-            </div>
-            <div className="hidden lg:p-5 lg:block">
-               <DataTablePagination pageSizes={[5, 10, 15]} table={table} />
-            </div>
-         </Card>
-      </div>
-   );
+            <Card className="h-full border-0 min-h-[70vh] bg-background dark:bg-secondary/30 ">
+                <div className="flex w-full px-5  justify-between pt-5 items-center lg:justify-start space-x-3">
+                    <div className="items-center w-full gap-x-4 text-muted-foreground">
+                        <div className="relative">
+                            <SearchIcon className="absolute w-4 h-auto top-3 left-2" />
+                            <Input
+                                ref={onFocusSearch}
+                                placeholder="Search..."
+                                value={globalFilter}
+                                onChange={(event) =>
+                                    setGlobalFilter(event.target.value)
+                                }
+                                className=" pl-8 bg-secondary border-0 border-b text-sm md:text-base ring-offset-0 focus-visible:ring-0 focus-visible:ring-offset-0"
+                            />
+                        </div>
+                    </div>
+                    <div className="">
+                        <DataTableViewOptions className="h-10 " table={table} />
+                    </div>
+                </div>
+                <DataTable
+                    headerLayout="bg-secondary"
+                    className="flex-1 min-h-[70vh] dark:bg-transparent rounded-2xl"
+                    isError={isError}
+                    isLoading={isLoading || isFetching}
+                    table={table}
+                />
+                <div className="lg:hidden p-5">
+                    <DataTableBasicPagination2 table={table} />
+                </div>
+                <div className="hidden lg:p-5 lg:block">
+                    <DataTablePagination
+                        pageSizes={[5, 10, 15]}
+                        table={table}
+                    />
+                </div>
+            </Card>
+        </div>
+    );
 };
 
 export default EventTable;
