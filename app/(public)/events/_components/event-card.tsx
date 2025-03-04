6f58c4d61440aaeecf6d272d291079520c053e10
Jervx <louellagracechua@gmail.com>
2025-03-04 17:18:07 +0800

    style: Add enhancements on events page

--- app/(public)/events/_components/event-card.tsx
+++ app/(public)/events/_components/event-card.tsx
@@ -1,8 +1,9 @@
-import React from "react";
-import Link from "next/link";
 import { format } from "date-fns";
-
-import { Calendar, MapPin } from "lucide-react";
+import { GiVote } from "react-icons/gi";
+import React, { useState } from "react";
+import { LuPartyPopper } from "react-icons/lu";
+import { motion, Variants } from "framer-motion";
+import { ArrowRight, Calendar, MapPin } from "lucide-react";
 
 import { Button } from "@/components/ui/button";
 
@@ -14,44 +15,137 @@ type Props = {
     className?: string;
 };
 
+const cardVariants: Variants = {
+    hidden: { opacity: 0, y: 10 },
+    visible: {
+        y: 0,
+        opacity: 1,
+        transition: {
+            duration: 0.2,
+            bounce: 0.8,
+        },
+    },
+};
+
+const innerCardVariant: Variants = {
+    initial: { y: 0.2 },
+    hover: {
+        scale: 1.02,
+        y: 0,
+        transition: {
+            duration: 0.2,
+            ease: "easeIn",
+        },
+    },
+};
+
+const linkVariant: Variants = {
+    shown: {
+        y: 0,
+        opacity: 1,
+        scale: 1.02,
+        transition: {
+            bounce: 0.4,
+            duration: 0.6,
+            type: "spring",
+        },
+    },
+    hidden: {
+        scale: 0.9,
+        y: 48,
+        opacity: 0.2,
+        transition: {
+            ease: "easeIn",
+        },
+    },
+};
+
 const EventCard = ({ className, event }: Props) => {
+    const [hover, setHover] = useState(false);
+
     return (
-        <div
-            className={cn(
-                "relative overflow-clip min-h-[500px] rounded-xl flex flex-col gap-y-4",
-                className
-            )}
-        >
-            <img
-                alt="Event Cover"
-                src={event.coverImage as ""}
-                className="absolute z-0 top-0 object-cover left-0 rounded-xl w-full h-full"
-            />
-            <div className="flex-auto" />
-            <div className="p-5 bg-popover/80 flex z-40 backdrop-blur-lg relative max-w-full flex-col w-full gap-y-4">
-                <h1 className="text-xl md:text-2xl max-w-full whitespace-nowrap overflow-hidden text-ellipsis dark:text-[#e7e0fb]">
-                    {event.title}
-                </h1>
-                <p className="text-foreground/80">{event.description}</p>
+        <motion.div variants={cardVariants}>
+            <motion.div
+                variants={innerCardVariant}
+                initial="initial"
+                whileHover="hover"
+                onClick={() => setHover(true)}
+                onPointerEnter={() => setHover(true)}
+                onPointerLeave={() => setHover(false)}
+                className={cn(
+                    "relative overflow-clip bg-secondary min-h-[500px] rounded-xl flex flex-col gap-y-4",
+                    className
+                )}
+            >
+                <motion.img
+                    alt="Event Cover"
+                    animate={
+                        hover
+                            ? { scale: 1.2, transition: { type: "spring" } }
+                            : {
+                                  scale: 1,
+                                  transition: {
+                                      type: "spring",
+                                      bounce: 0.2,
+                                      ease: "easeIn",
+                                  },
+                              }
+                    }
+                    src={event.coverImage as ""}
+                    className="absolute z-0 top-0 object-cover left-0 rounded-xl w-full h-full"
+                />
+                <div className="flex-auto" />
+                <div className="p-5 bg-popover/80 flex z-40 backdrop-blur-lg relative max-w-full flex-col w-full gap-y-4">
+                    <h1 className="text-xl md:text-2xl max-w-full whitespace-nowrap overflow-hidden text-ellipsis dark:text-[#e7e0fb]">
+                        {event.title}
+                    </h1>
+                    <p className="text-foreground/80">{event.description}</p>
 
-                <div className="flex flex-col gap-y-2">
-                    <p className="px-4 py-1 inline-flex items-center gap-x-4 rounded-full bg-secondary text-sm w-fit">
-                        <Calendar className="size-4" strokeWidth={1} />
-                        {format(event.date, "EEE, MMM dd, yyyy")}
-                    </p>
+                    <motion.div className="flex flex-col gap-y-2">
+                        <p className="px-4 py-1 inline-flex items-center gap-x-4 rounded-full bg-secondary text-sm w-fit">
+                            <Calendar className="size-4" strokeWidth={1} />
+                            {format(event.date, "EEE, MMM dd, yyyy")}
+                        </p>
 
-                    <p className="px-4 py-1 inline-flex items-center gap-x-4 rounded-full bg-secondary text-sm w-fit">
-                        <MapPin className="size-4" strokeWidth={1} />
-                        {event.location}
-                    </p>
+                        <p className="px-4 py-1 inline-flex items-center gap-x-4 rounded-full bg-secondary text-sm w-fit">
+                            <MapPin className="size-4" strokeWidth={1} />
+                            {event.location}
+                        </p>
+                        <p className="px-4 py-1 inline-flex items-center gap-x-4 rounded-full bg-secondary text-sm w-fit">
+                            {event.category === "event" ? (
+                                <LuPartyPopper
+                                    className="size-4"
+                                    strokeWidth={1}
+                                />
+                            ) : (
+                                <GiVote className="size-4" strokeWidth={1} />
+                            )}
+                            {event.category === "event"
+                                ? "Event"
+                                : "Election Event"}
+                        </p>
+                    </motion.div>
+                    <motion.a
+                        animate={hover ? "shown" : "hidden"}
+                        variants={linkVariant}
+                        href={`/events/${event.id}`}
+                        className="w-full hidden md:block"
+                    >
+                        <Button className=" bg-gradient-to-r group from-[#dce2d9] w-full to-[#7fbeed] hover:to-[#d8bce8] text-black/80 hover:text-black rounded-xl px-8 py-4">
+                            Go to Event <ArrowRight className="ml-4" />
+                        </Button>
+                    </motion.a>
+                    <a
+                        href={`/events/${event.id}`}
+                        className="w-full md:hidden block"
+                    >
+                        <Button className=" bg-gradient-to-r group from-[#dce2d9] w-full to-[#7fbeed] hover:to-[#d8bce8] text-black/80 hover:text-black rounded-xl px-8 py-4">
+                            Go to Event <ArrowRight className="ml-4" />
+                        </Button>
+                    </a>
                 </div>
-                <Link href={`/events/${event.id}`} className="w-full md:w-fit">
-                    <Button className="bg-gradient-to-r from-[#dce2d9] w-full md:w-fit to-[#7fbeed] hover:to-[#d8bce8] text-black/80 hover:text-black rounded-xl px-8 py-4">
-                        Go to Event
-                    </Button>
-                </Link>
-            </div>
-        </div>
+            </motion.div>
+        </motion.div>
     );
 };
 
--- app/(public)/events/page.tsx
+++ app/(public)/events/page.tsx
@@ -1,20 +1,30 @@
 export const dynamic = "force-dynamic";
 
 import React from "react";
+import Link from "next/link";
 
 import EventList from "./_components/event-list";
 import PartyPopperSvg from "@/components/custom-svg/party-popper";
 
 const EventsPage = async () => {
     return (
-        <div className="flex flex-col min-h-screen py-16 lg:py-0 w-full items-center">
-            <div className="flex flex-col gap-y-4 items-center px-8 py-16 ">
+        <div className="flex flex-col min-h-screen lg:py-0 w-full items-center">
+            <div className="flex text-sm py-4 mt-8 gap-y-4 items-center gap-x-4 text-foreground/50 w-full px-8 justify-between">
+                <Link href="/events">Events</Link>
+                <Link href="/">Home</Link>
+            </div>
+            <div className="flex flex-col gap-y-4 items-center px-8 mb-12 ">
                 <PartyPopperSvg className="size-[70px]" />
                 <p className="text-3xl font-medium md:text-4xl lg:text-5xl">
                     Upcoming Events
                 </p>
             </div>
             <EventList />
+            <div className="flex text-xs bg-secondary py-2 mt-8 gap-y-4 items-center gap-x-4 text-foreground/50 flex-col w-full px-8 md:flex-row md:justify-center">
+                <p>eCOOP</p>
+                <p>ACE System</p>
+                <p>Lands Horizon</p>
+            </div>
         </div>
     );
 };
